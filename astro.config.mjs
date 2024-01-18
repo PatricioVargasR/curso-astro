171d5af0062706f0638a844dfd4f109cd40664ad
PatricioVargasR <1722110158@utectulancingo.edu.mx>
2024-01-17 20:43:32 -0600

    END FIRST CLASS ASTRO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // Archivo de configuración de astro donde se pueden administrar las integraciones de diversos componentes
 
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), preact()],
+  output: 'hybrid'
 });
